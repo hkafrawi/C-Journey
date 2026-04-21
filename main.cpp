45c2096c6f30217e504e0c377f80f4872efb1492
Kafrawi <hassaballahelkafrawi@gmail.com>
2026-04-21 22:45:18 +0200

    Add: understanding memory location

--- main.cpp
+++ main.cpp
@@ -7,16 +7,13 @@ int main() {
         Cat cat1("Lilly",2,true);
         cat1.toString();
         cat1.speak(); //cat1 by default happy
-        //cout << cat1.toString() << endl;
-        //cout << "Name with get method " + cat1.getName() <<endl;
-        //cout << cat1.getAge() << endl;
+        cout<< "Memory Location of object: " << &cat1 <<endl;
     } // cat1 only live here
 
     Cat cat2("Misho",4,false);
-    //cat2.SetName("Misho");
     cat2.toString();
     cat2.speak();
-    //cout << cat2.toString() << endl;
+    cout<< "Memory Location of object: " << &cat2 <<endl;
 
     return 0;
 }
